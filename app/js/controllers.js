b8f21f5b73b8dd9fbf7478f47fa9bea49df52efb
Justin Robbins <justinhrobbins@gmail.com>
2014-03-02 10:08:35 -0700

    changed Filter into  real product search which runs incremental query against the API using search term

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,9 +4,15 @@
 
 var productControllers = angular.module('productControllers', []);
 
-productControllers.controller('ProductListCtrl', ['$scope', 'products',
-  function($scope, products) {
+productControllers.controller('ProductListCtrl', ['$scope', 'products', 'ProductSvc',
+  function($scope, products, ProductSvc) {
     $scope.searchResult = products;
     $scope.orderProp = 'name';
+
+    $scope.change = function() {
+        if ($scope.query.length > 1) {
+            $scope.searchResult = ProductSvc.query({query: $scope.query});
+        }
+    };
   }]);
 
