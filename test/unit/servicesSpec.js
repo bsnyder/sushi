465cc46f7ea9512d452b18adcf26fb2b817c42e3
Justin Robbins <justin.robbins@hybris.com>
2014-03-03 15:45:32 -0700

    added test for MultiProductLoader to servicesSpec

--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -1,7 +1,42 @@
 'use strict';
 
 /* jasmine specs for services go here */
+describe('Product services: ', function() {
 
-describe('service', function() {
+    beforeEach(function() {
+        this.addMatchers({
+            toEqualData: function(expected) {
+                return angular.equals(this.actual, expected);
+            }
+        });
+    });
 
+    beforeEach(module('productApp'));
+    beforeEach(module('productServices'));
+
+    describe('MultiProductLoader', function() {
+        var mockBackend, loader;
+
+        beforeEach(inject(function(_$httpBackend_, ProductSvc, MultiProductLoader) {
+            mockBackend = _$httpBackend_;
+            loader = MultiProductLoader;
+        }));
+
+        it('should load list of products', function() {
+            mockBackend.expectGET('http://responsive.hybris.com:9001/rest/v1/apparel-uk/products?pageSize=3').respond({"products":[{name: 'Shirt'}, {name: 'Hat'}]});
+
+            var products;
+
+            var promise = loader();
+            promise.then(function(prod) {
+                products = prod;
+            });
+
+            expect(products).toBeUndefined();
+
+            mockBackend.flush();
+
+            expect(products).toEqualData({"products":[{name: 'Shirt'}, {name: 'Hat'}]});
+        });
+    });
 });
