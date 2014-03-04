bac0c866d5d1a6e7a57adf938eb54d8feb4b368a
Justin Robbins <justin.robbins@hybris.com>
2014-03-04 10:08:27 -0700

    improve unit test coverage in the ProductListCtrl

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,69 +1,109 @@
 'use strict';
 
 /* jasmine specs for controllers go here */
-describe('Product controllers', function() {
+describe('Product controllers: ', function () {
 
-  beforeEach(function(){
-    this.addMatchers({
-      toEqualData: function(expected) {
-        return angular.equals(this.actual, expected);
-      }
-    });
-  });
-
-  beforeEach(module('productApp'));
-  beforeEach(module('productServices'));
-
-  describe('ProductListCtrl', function(){
-    var scope, ctrl, $httpBackend, products;
-
-    beforeEach(inject(function(_$httpBackend_, $rootScope, $controller, ProductSvc) {
-      $httpBackend = _$httpBackend_;
-
-      scope = $rootScope.$new();
-      ctrl = $controller('ProductListCtrl', {
-          $scope: scope,
-          products: {"products":[{name: 'Shirt'}, {name: 'Hat'}]}
-      });
-    }));
-
-
-    it('should have list of products', function() {
-      expect(scope.searchResult).toEqualData({"products":[{name: 'Shirt'}, {name: 'Hat'}]});
+    beforeEach(function () {
+        this.addMatchers({
+            toEqualData: function (expected) {
+                return angular.equals(this.actual, expected);
+            }
+        });
     });
 
+    beforeEach(module('productApp'));
+    beforeEach(module('productServices'));
 
-//    it('should set the default value of orderProp model', function() {
-//      expect(scope.orderProp).toBe('name');
-//    });
-  });
+    describe('ProductListCtrl', function () {
+        var scope, ctrl, $httpBackend, products;
 
-/*
-  describe('PhoneDetailCtrl', function(){
-    var scope, $httpBackend, ctrl,
-        xyzPhoneData = function() {
-          return {
-            name: 'phone xyz',
-                images: ['image/url1.png', 'image/url2.png']
-          }
-        };
+        beforeEach(inject(function (_$httpBackend_, $rootScope, $controller, ProductSvc) {
+            $httpBackend = _$httpBackend_;
 
+            scope = $rootScope.$new();
 
-    beforeEach(inject(function(_$httpBackend_, $rootScope, $routeParams, $controller) {
-      $httpBackend = _$httpBackend_;
-      $httpBackend.expectGET('data/xyz.json').respond(xyzPhoneData());
+            ctrl = $controller('ProductListCtrl', {
+                $scope: scope,
+                products: {"products":[{name: 'Shirt'}, {name: 'Hat'}]}});
+        }));
 
-      $routeParams.phoneId = 'xyz';
-      scope = $rootScope.$new();
-      ctrl = $controller('PhoneDetailCtrl', {$scope: scope});
-    }));
+        it('should have an initial list of products', function() {
+            expect(scope.searchResult).toEqualData({"products":[{name: 'Shirt'}, {name: 'Hat'}]});
+        });
 
+        it('should set the default value of orderProp model', function () {
+            expect(scope.orderProp).toBe('name');
+        });
 
-    it('should fetch phone detail', function() {
-      expect(scope.phone).toEqualData({});
-      $httpBackend.flush();
+        it('should set the default value of pageSize model', function () {
+            expect(scope.pageSize).toBe('3');
+        });
 
-      expect(scope.phone).toEqualData(xyzPhoneData());
+        it('should invoke search function on scope.searchChange()', function () {
+
+            // actual thing we want to test
+            $httpBackend.expectGET('http://responsive.hybris.com:9001/rest/v1/apparel-uk/products?currentPage=0&pageSize=5&query=foo').
+                respond({"products": [
+                    {name: 'Shirt'},
+                    {name: 'Hat'}
+                ]});
+
+            scope.searchModel = 'foo';
+            scope.pageSize = '5';
+
+            scope.searchChange();
+
+            $httpBackend.flush();
+            expect(scope.searchResult).toEqualData(
+                {"products": [
+                    {name: 'Shirt'},
+                    {name: 'Hat'}
+                ]});
+        });
+
+        it('should invoke search function on scope.pageSizeChange()', function () {
+
+            // actual thing we want to test
+            $httpBackend.expectGET('http://responsive.hybris.com:9001/rest/v1/apparel-uk/products?currentPage=0&pageSize=5&query=foo').
+                respond({"products": [
+                    {name: 'Shirt'},
+                    {name: 'Hat'}
+                ]});
+
+            scope.searchModel = 'foo';
+            scope.pageSize = '5';
+
+            scope.pageSizeChange();
+
+            $httpBackend.flush();
+            expect(scope.searchResult).toEqualData(
+                {"products": [
+                    {name: 'Shirt'},
+                    {name: 'Hat'}
+                ]});
+        });
+
+        it('should invoke search function on scope.setPage()', function () {
+
+            // actual thing we want to test
+            $httpBackend.expectGET('http://responsive.hybris.com:9001/rest/v1/apparel-uk/products?currentPage=1&pageSize=5&query=foo').
+                respond({"products": [
+                    {name: 'Shirt'},
+                    {name: 'Hat'}
+                ]});
+
+            scope.searchModel = 'foo';
+            scope.pageSize = '5';
+
+            scope.setPage(2);
+
+            $httpBackend.flush();
+            expect(scope.searchResult).toEqualData(
+                {"products": [
+                    {name: 'Shirt'},
+                    {name: 'Hat'}
+                ]});
+        });
+
     });
-  });*/
 });
