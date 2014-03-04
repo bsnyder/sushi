94a77b0ec9b962b3856ab4a3b70688169b5dc248
Justin Robbins <justin.robbins@hybris.com>
2014-03-04 15:35:58 -0700

    add hi-router

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -41,9 +41,9 @@ productControllers.controller('ProductListCtrl', ['$scope', 'products', 'MultiPr
         };
     }]);
 
-productControllers.controller('ProductDetailCtrl', ['$scope', '$routeParams', 'MultiProductLoader',
-    function ($scope, $routeParams, MultiProductLoader) {
-        MultiProductLoader.query({query: $scope.searchModel, code: $routeParams.code, options: 'DESCRIPTION'}).then(
+productControllers.controller('ProductDetailCtrl', ['$scope', '$stateParams', 'MultiProductLoader',
+    function ($scope, $stateParams, MultiProductLoader) {
+        MultiProductLoader.query({query: $scope.searchModel, code: $stateParams.code, options: 'DESCRIPTION'}).then(
             function(product){
                 $scope.product = product;
             });
