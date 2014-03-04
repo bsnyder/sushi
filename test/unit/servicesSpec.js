f38ffd2e48ebdaa39970ffbbd284c2b2aaca9803
Vera Coberley <vera.coberley@hybris.com>
2014-03-04 13:58:24 -0700

    get test to pass

--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -15,19 +15,20 @@ describe('Product services: ', function() {
     beforeEach(module('productServices'));
 
     describe('MultiProductLoader', function() {
-        var mockBackend, loader;
+        var mockBackend, loader, scope;
 
-        beforeEach(inject(function(_$httpBackend_, ProductSvc, MultiProductLoader) {
+        beforeEach(inject(function(_$httpBackend_, $rootScope, ProductSvc, MultiProductLoader) {
             mockBackend = _$httpBackend_;
             loader = MultiProductLoader;
+            scope = $rootScope.$new();
         }));
 
         it('should load list of products', function() {
             mockBackend.expectGET('http://responsive.hybris.com:9001/rest/v1/apparel-uk/products?pageSize=3').respond({"products":[{name: 'Shirt'}, {name: 'Hat'}]});
 
             var products;
-
-            var promise = loader();
+            scope.pageSize=3;
+            var promise = loader.query();
             promise.then(function(prod) {
                 products = prod;
             });
